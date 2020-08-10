0b26f08d5d8caf2416d41c99ec402d65096f6e61
jackass <Edes2128>
2020-08-10 22:20:03 +0200

    G

--- src/components/pages/website.jsx
+++ src/components/pages/website.jsx
@@ -177,7 +177,7 @@ const Website = () => {
             method: "POST",
             headers : {
                 "Content-Type": "application/json",
-                //"Access-Control-Allow-Origin":"*"
+                "Access-Control-Allow-Origin":"*"
         },
             body: JSON.stringify(payload)
     }).catch(e => console.log(e))
